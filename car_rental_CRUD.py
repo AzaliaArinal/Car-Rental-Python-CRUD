d2edb0a089e0dd21465f1a6e48a53546e8bd4857
AzaliaArinal <azaliarinal@gmail.com>
2024-06-25 20:49:49 +0700

    Add files via upload

--- car_rental_CRUD.py
+++ car_rental_CRUD.py
@@ -3,14 +3,16 @@
 # /========== AZALIA ANANDA ARINAL ==========/
 # /==========     RENTAL MOBIL     ==========/
 
-# DATA
+from datetime import datetime
+
+# /==================== DATA ====================/
 userpass = {
    'admin' : 'admin'
 }
 
 tabel_mobil = [
    {
-      'No_polisi' : 'L 1 A',
+      'no_polisi' : 'L 1 A',
       'Merek' : 'FIAT  ',
       'Tipe' :'FIAT 500',
       'Transmisi' : 'MT',
@@ -18,7 +20,7 @@ tabel_mobil = [
       'status' : 'TERSEDIA'
    },
    {
-      'No_polisi' : 'L 21 C',
+      'no_polisi' : 'L 21 C',
       'Merek' : 'TESLA',
       'Tipe' :'CYBERTRUCK',
       'Transmisi' : 'AT',
@@ -26,7 +28,7 @@ tabel_mobil = [
       'status' : 'TERSEDIA'
    },
    {
-      'No_polisi' : 'L 98 E',
+      'no_polisi' : 'L 98 E',
       'Merek' : 'PEUGEOT',
       'Tipe' :'205 GTI',
       'Transmisi' : 'MT',
@@ -62,43 +64,43 @@ tabel_customer = [
    }
 ]
 
-tabel_pinjam = [
-   {
-      'id_pinjam' : 'P-0001',
-      'nama_cust' : 'MISS ANYA FORGER',
-      'no_polisi' : 'L 21 C',
-      'tanggal_pinjam' : '2024-02-21',
-      'tanggal_kembali' : '2024-02-27',
-      'lama_sewa' : 7,
-      'total' : 7000000,
-      'pembayaran' : 'Cash'
-   }
-]
+tabel_transaksi= []
 
-tabel_kembali = [
-   {
-      'id_pinjam' : 'P-0001',
-      'nama_cust' : 'MISS ANYA FORGER',
-      'no_polisi' : 'L 21 C',
-      'tanggal_pinjam' : '2024-02-21',
-      'tanggal_kembali' : '2024-02-27',
-      'lama_sewa' : 7,
-      'lama_denda' : 3,
-      'total' : 7000000,
-      'pembayaran' : 'Cash'
-   }
-]
-
-def cari_mobil(cari_no_polisi):
-    hasil_cari = [i for i in tabel_mobil if i['No_polisi'].lower() == cari_no_polisi.lower()]
-    if not hasil_cari:
-        print("DATA MOBIL TIDAK DITEMUKAN:", cari_no_polisi)
-    else:
-        print('No.Polisi \t| Merek \t| Tipe \t\t| Transmisi \t| Harga \t| Status  |')
-        for i in hasil_cari:
-            print(f'{i['No_polisi']} \t| {i['Merek']} \t| {i['Tipe']} \t| {i['Transmisi']}\t\t| {i['Harga']} \t| {i['status']}|')
+# /==================== VALIDATION ====================/
+def validate_car_id(plate_numb):
+   for i in tabel_mobil:
+      if i['no_polisi'] == plate_numb:
+         return i
+   return None
 
 
+
+def validate_customer_id(customer_id):
+   for i in tabel_customer:
+      if i['customer_id'] == customer_id:
+         return True
+   return False
+
+
+
+def validate_rent_id(rent_id):
+   for transaction in tabel_transaksi:
+      if transaction['rent_id'] == rent_id:
+         return transaction
+   return None
+
+
+
+def validate_date(input_date):
+   try:
+      datetime.strptime(input_date, '%Y-%m-%d')
+      return True
+   except ValueError:
+      return False
+
+
+
+# /==================== READ ====================/
 
 def cari_customer(cari_nama_cust):
     hasil_cari_nama = [i for i in tabel_customer if i['Nama'].lower() == cari_nama_cust.lower()]
@@ -110,6 +112,23 @@ def cari_customer(cari_nama_cust):
             print(f'{i['customer_id']} \t| {i['Nama']} \t| {i['Alamat']} \t| {i['no_hp']}\t\t| {i['no_KTP']} \t| {i['no_SIM']}|')
 
 
+
+
+def cari_mobil(cari_no_polisi):
+    hasil_cari = [i for i in tabel_mobil if i['no_polisi'].lower() == cari_no_polisi.lower()]
+    if not hasil_cari:
+        print("DATA MOBIL TIDAK DITEMUKAN:", cari_no_polisi)
+    else:
+        print('No.Polisi \t| Merek \t| Tipe \t\t| Transmisi \t| Harga \t| Status  |')
+        for i in hasil_cari:
+            print(f'{i['no_polisi']} \t| {i['Merek']} \t| {i['Tipe']} \t| {i['Transmisi']}\t\t| {i['Harga']} \t| {i['status']}|')
+
+
+
+
+
+
+# /==================== UPDATE ====================/
 
 def update_customer(id_cust, nama_baru=None, alamat_baru=None, hp_baru=None, ktp_baru=None, sim_baru=None):
     for i in tabel_customer:
@@ -128,11 +147,9 @@ def update_customer(id_cust, nama_baru=None, alamat_baru=None, hp_baru=None, ktp
             return
     print("CUSTOMER TIDAK DITEMUKAN")
 
-
-
 def update_mobil(id_mobil, merek_baru=None, tipe_baru=None, transmisi_baru=None, harga_baru=None, status_baru=None):
     for i in tabel_mobil:
-        if i['No_polisi'] == id_mobil:
+        if i['no_polisi'] == id_mobil:
             if merek_baru:
                 i['Merek'] = merek_baru
             if tipe_baru:
@@ -146,28 +163,161 @@ def update_mobil(id_mobil, merek_baru=None, tipe_baru=None, transmisi_baru=None,
             print("DATA MOBIL BERHASIL DIUPDATE")
             return
     print("MOBIL TIDAK DITEMUKAN")
+
+
+
+
+# /==================== TRANSACTION ====================/
+def rent_transaction():
+   rent_id = input("Enter new rent ID: ")
+
+   # input plate number
+   while True:
+      plate_numb = input("Enter car's plate number: ")
+      i = validate_car_id(plate_numb)
+      if i and i['status'] == 'TERSEDIA':
+         break
+      else:
+         print("Car ID or car is not available. Try again")
+   
+   # input customer ID
+   while True:
+      customer_id = input("Enter customer ID: ")
+      if validate_customer_id(customer_id):
+         break
+      else:
+         print("Invalid customer ID. Try again")
+   
+   # input start and end date
+   while True:
+      start_date = input("Enter start rent date (YYYY-MM-DD): ")
+      if validate_date(start_date):
+         start_date = datetime.strptime(start_date, '%Y-%m-%d').date()
+         break
+      else:
+         print("Invalid date format. Try again")
+   
+   while True:
+      end_date = input("Enter start rent date (YYYY-MM-DD): ")
+      if validate_date(end_date):
+         end_date = datetime.strptime(end_date, '%Y-%m-%d').date()
+         if end_date >= start_date:
+            break
+         else:
+            print("End date must be greater than Start date. Try again")
+      else:
+         print("Invadil date format. Try again")
    
 
+   # count rental days and total amount
+   rental_days = (end_date - start_date).days + 1
+   total_amount = rental_days * i['Harga']
+
+   confirm_rent = input("Submit transaction? Y/N: ")
+   if confirm_rent == 'Y' or 'y':
+      i['status'] = 'NOT AVAILABLE'
+
+      transaction = {
+         'rent_id' : rent_id,
+         'plate_number' : plate_numb,
+         'customer_id' : customer_id,
+         'start_rent_count' : start_date.isoformat(),
+         'end_rent_count' : end_date.isoformat(),
+         'rent_days_count' : rental_days,
+         'total' : total_amount,
+         'late_fee' : 0
+      }
+      tabel_transaksi.append(transaction)
+      print("Rent transaction added")
+   else:
+      print("Cancel transaction")
 
 
+
+
+late_fee = 200000
+
+def calculate_late_fee(late_days, daily_rent):
+    daily_penalty = daily_rent * 0.1  # Assuming 10% of the daily rent as the penalty per day
+    return late_days * daily_penalty
+
+def return_transaction():
+   while True:
+      rent_id = input("Enter rent transaction ID: ")
+      transaction = validate_rent_id(rent_id)
+      if transaction:
+         break
+      else:
+         print("Invalid transaction ID. Try again")
+   
+
+   while True:
+      return_date = input("Enter return date (YYYY-MM-DD): ")
+      if validate_date(return_date):
+         return_date = datetime.strptime(return_date, '%Y-%m-%d').date()
+         break
+      else:
+         print("Invalid date format. Try again")
+
+
+   late_fee_count = 0
+   late_return_date = input("Enter late return date (YYYY-MM-DD), Enter to skip: ").strip()
+   if late_return_date:
+      while True:
+         if validate_date(late_return_date):
+            late_return_date = datetime.strptime(late_return_date, '%Y-%m-%d').date()
+            break
+         else:
+            print("Invadil date format. Try again")
+
+         if late_return_date > return_date:
+            late_days = (late_return_date - return_date).days
+            late_fee_count = late_days * late_fee
+
+   confirm_return = input("Submit transaction? Y/N: ")
+   if confirm_return == 'Y' or 'y':
+      transaction['late_fee'] = late_fee_count
+
+      car = validate_car_id(transaction['plate_number'])
+      car['status'] = 'AVAILABLE'
+
+      print ("Return transaction added")
+   else:
+      print("Cancel transaction")
+   
+   
+   
+   
+# /==================== SHOW TABLES ====================/
+def show_transaction_table():
+   if len(tabel_transaksi) == 0:
+      print("\nThere is no transaction data\n")
+   else:
+      print("==========TABEL DATA CUSTOMER==========\n")
+      print('|Rent ID\t| Plate Number \t| Customer ID \t| Start Date \t| End Date \t| Days  | Late Fee | Total Amount')
+      for i in range(len(tabel_transaksi)):
+         print(f'| {tabel_transaksi[i]['rent_id']} \t| {tabel_transaksi[i]['plate_number']} \t| {tabel_transaksi[i]['customer_id']} \t| {tabel_transaksi[i]['start_rent_count']}\t| {tabel_transaksi[i]['end_rent_count']}\t| {tabel_transaksi[i]['rent_days_count']}\t| {tabel_transaksi[i]['late_fee']}\t\t| {tabel_transaksi[i]['total']}')
+
+
+
+
+
+# /==================== MENU ====================/
 def menu():
    while True:
       print("==========MENU UTAMA==========")
-      print("1. PINJAM MOBIL")
-      print("2. PENGEMBALIAN MOBIL")
-      print("3. DATA CUSTOMER")
-      print("4. DATA MOBIL")
-      print("5. LOGOUT")
+      print("1. TRANSAKSI")
+      print("2. DATA CUSTOMER")
+      print("3. DATA MOBIL")
+      print("4. LOGOUT")
       pilihan_menu = input("MASUKKAN PILIHAN MENU: ")
       if pilihan_menu == '1':
-         pinjam_mobil()
+         transaksi()
       elif pilihan_menu == '2':
-         pengembalian_mobil()
-      elif pilihan_menu == '3':
          data_customer()
-      elif pilihan_menu == '4':
+      elif pilihan_menu == '3':
          data_mobil()
-      elif pilihan_menu == '5':
+      elif pilihan_menu == '4':
          print("LOGGING OUT")
          exit()
       else:
@@ -177,60 +327,32 @@ def menu():
 
 
 
-def pinjam_mobil():
-   # while True:
-      print("==========MENU PINJAM MOBIL==========")
-      print("1. BUAT TRANSAKSI BARU")
+def transaksi():
+   while True:
+      print("\n==========MENU TRANSAKSI==========")
+      print("1. BUAT TRANSAKSI PINJAM")
+      print("2. BUAT TRANSAKSI KEMBALI")
       print("0. BACK TO MAIN MENU")
-      print("TABEL DATA CUSTOMER\n")
-      print('|ID Pinjam\t| NamaCust \t\t\t| Tanggal Pinjam \t| Tanggal Kembali \t\t| Lama sewa \t| Pembayaran|')
-      for i in range(len(tabel_pinjam)):
-         print(f'| {tabel_pinjam[i]['id_pinjam']} \t| {tabel_pinjam[i]['nama_cust']} \t| {tabel_pinjam[i]['no_polisi']} \t| {tabel_pinjam[i]['tanggal_pinjam']}\t| {tabel_pinjam[i]['tanggal_kembali']} | {tabel_pinjam[i]['lama_sewa']}\t| {tabel_pinjam[i]['total']}\t| {tabel_pinjam[i]['pembayaran']}\t|')
-      pilihan_pinjam = input("MASUKKAN PILIHAN MENU: ")
-      if pilihan_pinjam == '1':
-         konfirmasi = input("BUAT TRANSAKSI PINJAM? Y/N: ")
-         if konfirmasi == 'Y' or 'y':
-            # start_date_obj = datetime.strptime(start_date, "%Y-%m-%d")
-            # end_date_obj = datetime.strptime(end_date, "%Y-%m-%d")
-            # rental_days = (end_date_obj - start_date_obj).days + 1
-            # total_amount = rental_days * daily_rental_rate
-            print("TRANSAKSI BERHASIL DIBUAT")
-            # pinjam_mobil()
-         elif konfirmasi == 'N' or 'n':
-            # pinjam_mobil()
-            print("KEMBALI KE MENU PINJAM MOBIL")
-         else:
-            print("MASUKKAN PILIHAN LAGI")
-      elif pilihan_pinjam == '0':
+
+      show_transaction_table()
+
+      pilihan_transaksi = input("MASUKKAN PILIHAN MENU: ")
+            
+      if pilihan_transaksi == '1':
+         print("\n==========TRANSAKSI PINJAM==========")
+         rent_transaction()
+      
+      
+      if pilihan_transaksi == '2':
+         print("\n==========TRANSAKSI KEMBALI==========")
+         return_transaction()
+      
+      elif pilihan_transaksi == '0':
          menu()
       else:
          print("MASUKKAN PILIHAN LAGI")
+   
 
-
-
-
-
-def pengembalian_mobil():
-   # while True:
-      print("==========MENU PENGEMBALIAN MOBIL==========")
-      print("1. BUAT TRANSAKSI BARU")
-      print("0. BACK TO MAIN MENU")
-      print("tabel TRANSAKSI PINJAM")
-      for i in range(len(tabel_kembali)):
-         print(f'| {tabel_kembali[i]['id_pinjam']} \t| {tabel_kembali[i]['nama_cust']} \t| {tabel_kembali[i]['no_polisi']} \t| {tabel_kembali[i]['tanggal_pinjam']}\t| {tabel_kembali[i]['tanggal_kembali']} | {tabel_kembali[i]['lama_sewa']}\t| {tabel_kembali[i]['lama_denda']}\t| {tabel_kembali[i]['total']}\t| {tabel_kembali[i]['pembayaran']}\t|')
-      pilihan_kembali = input("MASUKKAN PILIHAN MENU: ")
-      if pilihan_kembali == '1':
-         konfirmasi_kembali = input("BUAT TRANSAKSI PENGEMBALIAN MOBIL? Y/N: ")
-         if konfirmasi_kembali == 'Y' or 'y':
-            print("TRANSAKSI BERHASIL DIBUAT")
-         elif konfirmasi_kembali == 'N' or 'n':
-            print("KEMBALI KE MENU UTAMA")
-         else:
-            print("MASUKKAN PILIHAN LAGI")
-      elif pilihan_kembali == '0':
-         menu()
-      else:
-         print("MASUKKAN PILIHAN LAGI")
 
 
 
@@ -274,6 +396,7 @@ def data_customer():
          print("MASUKKAN PILIHAN LAGI")
    
    
+
    elif pilihan_4 == '2':
       print("==========CARI DATA CUSTOMER==========")
       # print('''CARI BERDASARKAN: 
@@ -282,14 +405,8 @@ def data_customer():
       # print("MASUKKAN ID CUSTOMER: ")
       cari_nama_cust = input("MASUKKAN NAMA CUSTOMER: ")
       cari_customer(cari_nama_cust)
-      # konfirmasi_4 = input("CARI LAGI? Y/N: ")
-      # if konfirmasi_4 == 'Y' or 'y':
-      #    print("TRANSAKSI BERHASIL")
-      # elif konfirmasi_4 == 'N' or 'n':
-      #    print("KEMBALI KE MENU UTAMA")
-      # else:
-      #    print("MASUKKAN PILIHAN LAGI")
    
+
    
    elif pilihan_4 == '3':
       print("==========UBAH DATA CUSTOMER==========")
@@ -307,6 +424,7 @@ def data_customer():
       else:
          print("MASUKKAN PILIHAN LAGI")
    
+
    
    elif pilihan_4 == '4':
       print("==========HAPUS DATA CUSTOMER==========")
@@ -321,10 +439,12 @@ def data_customer():
          print("MASUKKAN PILIHAN LAGI")
    
    
+
    elif pilihan_4 == '0':
       menu()
    else:
       print("MASUKKAN PILIHAN LAGI")
+
 
 
 
@@ -340,7 +460,7 @@ def data_mobil():
    print("TABEL DATA MOBIL\n")
    print('|Index\t| No.Polisi \t| Merek \t| Tipe \t\t| Transmisi \t| Harga \t| Status  |')
    for i in range(len(tabel_mobil)):
-      print(f'|{i} \t| {tabel_mobil[i]['No_polisi']} \t| {tabel_mobil[i]['Merek']} \t| {tabel_mobil[i]['Tipe']} \t| {tabel_mobil[i]['Transmisi']}\t\t| {tabel_mobil[i]['Harga']} \t| {tabel_mobil[i]['status']}|')
+      print(f'|{i} \t| {tabel_mobil[i]['no_polisi']} \t| {tabel_mobil[i]['Merek']} \t| {tabel_mobil[i]['Tipe']} \t| {tabel_mobil[i]['Transmisi']}\t\t| {tabel_mobil[i]['Harga']} \t| {tabel_mobil[i]['status']}|')
    pilihan_5 = input("MASUKKAN PILIHAN MENU: ")
    
    
@@ -355,7 +475,7 @@ def data_mobil():
       konfirmasi_5 = input("SUBMIT? Y/N: ")
       if konfirmasi_5 == 'Y' or 'y':
          tabel_mobil.append({
-            'No_polisi' : no_polisi,
+            'no_polisi' : no_polisi,
             'Merek' : merek,
             'Tipe' : tipe,
             'Transmisi' : transmisi,
@@ -376,13 +496,6 @@ def data_mobil():
       #    ''')
       cari_no_polisi = input("MASUKKAN NO. POLISI: ")
       cari_mobil(cari_no_polisi)
-      # konfirmasi_5 = input("CARI LAGI? Y/N: ")
-      # if konfirmasi_5 == 'Y' or 'y':
-      #    print("TRANSAKSI BERHASIL")
-      # elif konfirmasi_5 == 'N' or 'n':
-      #    print("KEMBALI KE MENU UTAMA")
-      # else:
-      #    print("MASUKKAN PILIHAN LAGI")
 
 
    elif pilihan_5 == '3':
@@ -419,30 +532,6 @@ def data_mobil():
       menu()
    else:
       print("MASUKKAN PILIHAN LAGI")
-
-
-
-
-
-# def user_pass(user_pass, username, password):
-#    if username in userpass:
-#       if userpass[username] == password:
-#          return "SUCCESSFUL"
-#       else:
-#          return "TRY AGAIN"
-#    else:
-#       return "INVALID INPUT"
-
-# def login():
-#    username = input("USERNAME: ")
-#    password = input("PASSWORD: ")
-#    try_login = user_pass(user_pass, username, password)
-#    print(try_login)
-#    if try_login == "SUCCESSFUL":
-#       menu()
-
-# if __name__ == "__main__":
-#    login()
 
 if __name__ == "__main__":
    menu()
